34dc5bea32a2d00b2a7765e23b10da85ebe8bce3
atony2099 <atony2099@gmail.com>
2017-08-01 09:20:52 +0800

    模块化

---
+++ 06CSS模块化/src/js/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+var footerCss  =  require("../css/footer.css");
+
+
+export default class ComponentFooter extends React.Component {
+   render(){
+     return (
+       <footer>
+
+         <h1>尾部信</h1>
+       </footer>
+     )
+   }
+}
